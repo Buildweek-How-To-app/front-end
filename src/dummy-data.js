8e1fdf39921506b09df0165c7b79eb1a61c69d88
Lukecp <lukecp@pm.me>
2020-12-19 00:08:27 -0500

    added dummy data

---
+++ src/dummy-data.js
@@ -0,0 +1,19 @@
+const dummyData = [
+	{
+			id: 1,
+			title: 'Creating a How-to web app',
+			content: 'First, use create-react-app to get some boilerplate code working. Add any additional dependencies needed and start writing some dummy content to work with. When using Github, don\'t forget to "git pull" often so you have up to date changes from your team.',
+	},
+	{
+		id: 2,
+		title: 'Solve a Rubik\'s Cube',
+		content: 'Step 1 - Solve the white cross. Step 2 - Solve the white corners. Step 3 - Solve the middle layer. Step 4 - Solve the top face. Step 5 - Solve the final layer',
+	},
+	{
+		id: 3,
+		title: 'Replace a smartphone battery',
+		content: 'Remove screws on outer case and gently pull the screen away from the body. Disconnect the battery and disconnect the display if it will be in the way of your repair. Be careful not to damage anything. You may have to apply heat to remove the battery from it\' adhesive. Add in your new battery, reconnect all connections, startup and test. New batteies should be calibrated by fully draining it, charging it to 100%, then draining to 0% again before regular use.',
+	},
+];
+
+export default dummyData;
