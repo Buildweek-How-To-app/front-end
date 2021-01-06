c2815e6576d377bba21ab132c4956772c249dca2
Lukecp <lukecp@pm.me>
2021-01-06 00:29:36 -0500

    Created Edit Topic component

--- src/components/TopicPage.js
+++ src/components/TopicPage.js
@@ -1,34 +1,37 @@
 import React from 'react';
-import { Route, Link, useParams } from 'react-router-dom';
+import { Route, Link, useParams, useRouteMatch } from 'react-router-dom';
 import { Button } from 'reactstrap';
-import EditTopic from './EditTopic'
+import EditTopic from './EditTopic';
 import './TopicPage.css'
 
 
 const TopicPage = (props) => {
 	const params = useParams();
 	const topicItem = props.items.find(item => item.id === Number(params.id));
+	const routeMatch = useRouteMatch();
 
 	return (
 		<div>
-			<Route exact path={`/topics/${topicItem.id}/edit`} render={() => <EditTopic item={topicItem} />} />
 			
 			<Link to="/">
 				<Button color='primary'>Home</Button>
 			</Link>
+
 			<div className="Topic-Content">
 				<h1>{topicItem.title}</h1>
-
 				<p>{topicItem.content}</p>
 			</div>
 
-			<Link to={`/topics/${topicItem.id}/edit`}>
+			<Link to={`${routeMatch.url}/edit`}>
 				<Button color='warning'>Edit</Button>
 			</Link>
 
 			<Link to="/">
 				<Button color='danger'>Delete</Button>
 			</Link>
+
+			<Route exact path={`${routeMatch.path}/edit`} render={() => <EditTopic item={topicItem} />} />
+
 		</div>
 	);
 };
